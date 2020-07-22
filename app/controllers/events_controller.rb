3832f1d1c4afd547d1424d61b05b681bbff01082
yuichiro watanabe <yuuichiro1139@gmail.com>
2020-07-22 19:39:40 +0900

    カレンダー上の動きの追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,28 +28,21 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      redirect_to root_path, notice: 'Event was successfully created.'
+    else
+      render :new
     end
+
   end
 
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.update(event_params)
+      redirect_to root_path, notice: 'Event was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -57,10 +50,7 @@ class EventsController < ApplicationController
   # DELETE /events/1.json
   def destroy
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_path, notice: 'Event was successfully destroyed.'
   end
 
   private
--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -19,14 +19,38 @@ export default class extends Controller {
       headerToolbar: { center: 'dayGridMonth,timeGridWeek,timeGridDay' },
       plugins: [dayGridPlugin,timeGridPlugin,interactionPlugin],
       navLinkDayClick: function(date, jsEvent) {
+        _this.modalTarget.style.display = "block"
         _this.start_timeTarget.value = date
         _this.end_timeTarget.value = date
       },
       eventClick: function (info) {
         info.jsEvent.preventDefault()
         Turbolinks.visit(info.event.extendedProps.show_url)
+      },
+      eventDrop: function (info){
+        let data = _this.data(info)
+        Rails.ajax({
+          type: 'PUT',
+          url: info.event.url,
+          data: new URLSearchParams(data).toString()
+        })
+      },
+      eventResize: function (info) {
+        let data = _this.data(info)
+        Rails.ajax({
+          type: 'PUT',
+          url: info.event.url,
+          data: new URLSearchParams(data).toString()
+        })
       }
     })
     calendar.render()
+  }
+
+  data(info) {
+    return {
+      "event[start_time]": info.event.start,
+      "event[end_time]": info.event.end
+    }
   }
 }
